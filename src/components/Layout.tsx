411746617526aeac7c5d9871896e701b1bc02b37
sungwoo <54757435+sung-woo-jang@users.noreply.github.com>
2024-12-15 02:45:23 +0900

    temp

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -21,52 +21,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 내 트리를 꾸며줘🎄
               </Link>
             </div>
-            <div className='hidden md:flex items-center space-x-4'>
-              <Link
-                href='/'
-                className='text-gray-700 hover:text-indigo-600 transition duration-300'
-              >
-                홈
-              </Link>
-              <Link
-                href='/photozone'
-                className='text-gray-700 hover:text-indigo-600 transition duration-300'
-              >
-                포토존
-              </Link>
-            </div>
-            <div className='md:hidden flex items-center'>
-              <button
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-                className='text-gray-700 hover:text-indigo-600 focus:outline-none focus:text-indigo-600'
-              >
-                {isMenuOpen ? (
-                  <X className='h-6 w-6' />
-                ) : (
-                  <Menu className='h-6 w-6' />
-                )}
-              </button>
-            </div>
           </div>
         </div>
-        {isMenuOpen && (
-          <div className='md:hidden absolute top-16 left-0 w-full bg-white shadow-lg z-50'>
-            <div className='px-4 py-2 space-y-2'>
-              <Link
-                href='/'
-                className='block text-gray-700 hover:text-indigo-600 transition duration-300 bg-gray-100 rounded-lg px-4 py-2 hover:bg-indigo-50'
-              >
-                홈
-              </Link>
-              <Link
-                href='/photozone'
-                className='block text-gray-700 hover:text-indigo-600 transition duration-300 bg-gray-100 rounded-lg px-4 py-2 hover:bg-indigo-50'
-              >
-                포토존
-              </Link>
-            </div>
-          </div>
-        )}
       </nav>
       <main className='container mx-auto px-4 sm:px-6 lg:px-8 py-8'>
         {children}

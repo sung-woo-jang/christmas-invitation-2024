bfa7183f5f274523ae3f8490c47d2576ef12679d
sungwoo <54757435+sung-woo-jang@users.noreply.github.com>
2024-12-15 02:55:53 +0900

    temp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,21 +7,36 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: '내 트리를 꾸며줘🎄',
   description: '예수님의 생일파티에 초대합니다.',
-  metadataBase: new URL('https://christmas-invitations.vercel.app/'), // 실제 도메인으로 변경
+  // openGraph: {
+  //   title: '내 트리를 꾸며줘🎄',
+  //   description:
+  //     '예수님의 탄생을 축하하는 특별한 크리스마스 행사에 여러분을 초대합니다.',
+  //   type: 'website',
+  //   locale: 'ko_KR',
+  //   url: '/', // 또는 실제 페이지 경로
+  //   siteName: '내 트리를 꾸며줘🎄',
+  //   images: [
+  //     {
+  //       url: 'https://christmas-invitations.vercel.app/123.jpeg',
+  //       width: 3024,
+  //       height: 4032,
+  //       alt: '내 트리를 꾸며줘🎄 미리보기',
+  //     },
+  //   ],
+  // },
+
   openGraph: {
     title: '내 트리를 꾸며줘🎄',
     description:
       '예수님의 탄생을 축하하는 특별한 크리스마스 행사에 여러분을 초대합니다.',
     type: 'website',
     locale: 'ko_KR',
-    url: '/', // 또는 실제 페이지 경로
-    siteName: '내 트리를 꾸며줘🎄',
     images: [
       {
-        url: 'https://christmas-invitations.vercel.app/123.jpeg',
-        width: 3024,
-        height: 4032,
-        alt: '내 트리를 꾸며줘🎄 미리보기',
+        url: '/123.jpeg',
+        width: 1200,
+        height: 630,
+        alt: '크리스마스 초대장 미리보기',
       },
     ],
   },
